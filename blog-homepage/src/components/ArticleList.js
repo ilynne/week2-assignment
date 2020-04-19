d7ce2b5b81db661e67559669e50d924e1f6f7b31
Lynne Cooney <ilynne@gmail.com>
2020-04-19 11:04:28 -0700

    converts ArticleList to stateless functional component

--- blog-homepage/src/components/ArticleList.js
+++ blog-homepage/src/components/ArticleList.js
@@ -1,25 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Article from './Article';
 
-class ArticleList extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className={this.props.className}>
-        <div className={'heading-container'}>
-          <h1>{this.props.heading}</h1>
-        </div>
-        <div className={'article-list-container'}>
-          {this.props.articleListData.map((articleData, i) =>
-            <Article articleData={articleData}></Article>
-          )}
-        </div>
+const ArticleList = (props) => {
+  return (
+    <div className={props.className}>
+      <div className={'heading-container'}>
+        <h1>{props.heading}</h1>
       </div>
-    );
-  }
+      <div className={'article-list-container'}>
+        {props.articleListData.map((articleData, i) =>
+          <Article
+            articleData={articleData}
+            key={i}
+          >
+          </Article>
+        )}
+      </div>
+    </div>
+  );
+}
+
+ArticleList.propTypes = {
+  heading: PropTypes.string.isRequired,
+  articleListData: PropTypes.arrayOf(PropTypes.object),
 }
 
 export default ArticleList;
