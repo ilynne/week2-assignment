0fac0642af155d3a5445e099ad2e57bfc296c5d6
Lynne Cooney <ilynne@gmail.com>
2020-04-19 18:13:27 -0700

    Updates

---
+++ precache-manifest.8edd38a0d73f74aa0515b20e3c7b1ede.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f702d85d7d5e78ef053e88a10cf8c03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1738490fc9cf8f15030",
+    "url": "/static/css/main.0ba440ac.chunk.css"
+  },
+  {
+    "revision": "2a98d71b9dd3a365bedf",
+    "url": "/static/js/2.e1ca4f0b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e1ca4f0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1738490fc9cf8f15030",
+    "url": "/static/js/main.f0c9d413.chunk.js"
+  },
+  {
+    "revision": "b440b20bb297df52055a",
+    "url": "/static/js/runtime-main.f15a5295.js"
+  }
+]);
